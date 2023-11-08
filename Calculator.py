d0816003b1ef43326ce81c38a31dbe0cb9501d28
Almfueb <rein.cornelius@gmail.com>
2023-11-08 16:58:53 +0700

    last last commit

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 def button_press(num):
 
@@ -7,6 +8,7 @@ def button_press(num):
         num = '*'
     elif num == '÷':
         num = '/'
+    equation_label.set(equation_text)
     equation_text = equation_text + str(num)
     equation_label.set(equation_text)
 
@@ -15,10 +17,13 @@ def equals():
     global equation_text
 
     try:
-
-        total = str(eval(equation_text))
+        total = eval(equation_text)
+        if total.is_integer():
+            total = int(total)
+        else:
+            total = round(total, 2)
         equation_label.set(total)
-        equation_text = total
+        equation_text = str(total)
 
     except SyntaxError:
         equation_label.set("syntax error")
@@ -49,6 +54,28 @@ def backspace():
     equation_text = equation_text[:-1]
     equation_label.set(equation_text)
 
+def factorial():
+    global equation_text
+    try:
+        result = math.factorial(int(equation_text))
+        equation_label.set(result)
+        equation_text = str(result)
+    except ValueError:
+        equation_label.set("Invalid input for factorial")
+    except Exception as e:
+        equation_label.set("Error")
+        equation_text = ""
+
+def kuadrat():
+    global equation_text
+    try:
+        result = eval(equation_text)**2
+        equation_label.set(result)
+        equation_text = str(result)
+    except:
+        equation_label.set("Error")
+        equation_text = ""
+
 def evaluate(event=None):
     equals()
 
@@ -71,7 +98,7 @@ def key_press(event):
 
 window = Tk()
 window.title("Calculator program")
-window.geometry("500x550")
+window.geometry("500x500")
 
 equation_text = ""
 
@@ -87,57 +114,66 @@ frame.pack(pady=10)
 window.bind("<Key>", key_press)
 
 button1 = Button(frame, text=1, height=4, width=9, font=35,command=lambda: button_press(1))
-button1.grid(row=0, column=0)
+button1.grid(row=0, column=1)
 
 button2 = Button(frame, text=2, height=4, width=9, font=35,command=lambda: button_press(2))
-button2.grid(row=0, column=1)
+button2.grid(row=0, column=2)
 
 button3 = Button(frame, text=3, height=4, width=9, font=35,command=lambda: button_press(3))
-button3.grid(row=0, column=2)
+button3.grid(row=0, column=3)
 
 button4 = Button(frame, text=4, height=4, width=9, font=35,command=lambda: button_press(4))
-button4.grid(row=1, column=0)
+button4.grid(row=1, column=1)
 
 button5 = Button(frame, text=5, height=4, width=9, font=35,command=lambda: button_press(5))
-button5.grid(row=1, column=1)
+button5.grid(row=1, column=2)
 
 button6 = Button(frame, text=6, height=4, width=9, font=35,command=lambda: button_press(6))
-button6.grid(row=1, column=2)
+button6.grid(row=1, column=3)
 
 button7 = Button(frame, text=7, height=4, width=9, font=35,command=lambda: button_press(7))
-button7.grid(row=2, column=0)
+button7.grid(row=2, column=1)
 
 button8 = Button(frame, text=8, height=4, width=9, font=35,command=lambda: button_press(8))
-button8.grid(row=2, column=1)
+button8.grid(row=2, column=2)
 
 button9 = Button(frame, text=9, height=4, width=9, font=35,command=lambda: button_press(9))
-button9.grid(row=2, column=2)
+button9.grid(row=2, column=3)
 
 button0 = Button(frame, text=0, height=4, width=9, font=35,command=lambda: button_press(0))
-button0.grid(row=3, column=1)
+button0.grid(row=3, column=2)
 
 plus = Button(frame, text='+', height=4, width=9, font=35,command=lambda: button_press('+'))
-plus.grid(row=0, column=3)
+plus.grid(row=0, column=4)
 
 minus = Button(frame, text='-', height=4, width=9, font=35,command=lambda: button_press('-'))
-minus.grid(row=1, column=3)
+minus.grid(row=1, column=4)
 
 multiply = Button(frame, text='x', height=4, width=9, font=35, command=lambda: button_press('x'))
-multiply.grid(row=2, column=3)
+multiply.grid(row=2, column=4)
 
 divide = Button(frame, text='÷', height=4, width=9, font=35,command=lambda: button_press('/'))
-divide.grid(row=3, column=3)
+divide.grid(row=3, column=4)
 
-decimal = Button(frame, text='.', height=4, width=9, font=35,command=lambda: button_press('.'))
-decimal.grid(row=3, column=0)
+decimal = Button(frame, text=',', height=4, width=9, font=200,command=lambda: button_press(','))
+decimal.grid(row=3, column=4)
 
 clear = Button(frame, text='clear', height=4, width=9, font=35,command=clear_label)
-clear.grid(row=3, column=2)
+clear.grid(row=3, column=3)
 
-percent = Button(frame, text='%', height=4, width=11, font=35, command=percent)
-percent.grid(row=4, column=0, columnspan=2)
+percent = Button(frame, text='%', height=4, width=9, font=35, command=percent)
+percent.grid(row=0, column=0)
 
-equal = Button(frame, text='=', height=4, width=11, font=35, command=equals)
-equal.grid(row=4, column=2, columnspan=2)
+equal = Button(frame, text='=', height=4, width=9, font=35, command=equals)
+equal.grid(row=3, column=1)
+
+delete_button = Button(frame, text='Del', height=4, width=9, font=35, command=backspace)
+delete_button.grid(row=3, column=0)
+
+factorial_button = Button(frame, text='!', height=4, width=9, font=35, command=factorial)
+factorial_button.grid(row=2, column=0)
+
+kuadrat_button = Button(frame, text='^2', height=4, width=9, font=35, command=kuadrat)
+kuadrat_button.grid(row=1, column=0)
 
 window.mainloop()
